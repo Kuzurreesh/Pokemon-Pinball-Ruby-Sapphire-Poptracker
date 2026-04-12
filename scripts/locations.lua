198d2a43b9e596c68b63ddf4d89306cf4a74bd3d
Kuzurreesh <nino-077@hotmail.com>
2026-04-12 14:55:03 +0200

    deleted:    .github/workflows/release.yml

--- scripts/locations.lua
+++
@@ -1,4 +0,0 @@
-Tracker:AddLocations("locations/Ruby Board.json")
-Tracker:AddLocations("locations/Sapphire Board.json")
-Tracker:AddLocations("locations/Eggs - Special.json")
-Tracker:AddLocations("locations/Evolutions.json")
